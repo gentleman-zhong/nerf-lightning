6ef4a1aac9260c754786ac8701c08a7acd2d19ec
gentleman-zhong <1152253080@qq.com>
2023-10-13 10:58:32 +0800

    first commit

---
+++ train.py
@@ -0,0 +1,187 @@
+import torchvision
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch
+import torch.utils.data as data
+from collections import defaultdict
+
+
+from lightning.pytorch.callbacks import ModelCheckpoint, TQDMProgressBar, ModelSummary
+from lightning.pytorch.loggers import TensorBoardLogger
+from lightning import LightningModule
+from lightning.pytorch import Trainer, LightningDataModule
+
+
+from load_datasets import dataset_dict
+from losses import loss_dict
+from models.nerf import *
+from utils.utils import *
+from models.rendering import *
+import opt
+
+
+class NeRFLightningModule(LightningModule):
+    def __init__(self, hparams):
+        super().__init__()
+        self.save_hyperparameters(hparams)
+        self.example_input_array = torch.Tensor(640000, 8)
+        self.val_outputs = {'val_loss': [], 'val_psnr': []}
+
+
+        self.loss = loss_dict['color'](coef=1)
+
+        self.embedding_xyz = Embedding(hparams.N_emb_xyz)
+        self.embedding_dir = Embedding(hparams.N_emb_dir)
+        self.embeddings = {'xyz': self.embedding_xyz,
+                           'dir': self.embedding_dir}
+
+        self.nerf_coarse = NeRF(in_channels_xyz=6 * hparams.N_emb_xyz + 3,
+                                in_channels_dir=6 * hparams.N_emb_dir + 3)
+        self.models = {'coarse': self.nerf_coarse}
+        load_ckpt(self.nerf_coarse, hparams.weight_path, 'nerf_coarse')
+
+        if hparams.N_importance > 0:
+            self.nerf_fine = NeRF(in_channels_xyz=6 * hparams.N_emb_xyz + 3,
+                                  in_channels_dir=6 * hparams.N_emb_dir + 3)
+            self.models['fine'] = self.nerf_fine
+            load_ckpt(self.nerf_fine, hparams.weight_path, 'nerf_fine')
+
+    def forward(self, rays):
+        num_rays = rays.shape[0]
+        results = defaultdict(list)
+        for i in range(0, num_rays, self.hparams.chunk):
+            rendered_ray_chunks = \
+                render_rays(self.models,
+                            self.embeddings,
+                            rays[i:i + self.hparams.chunk],
+                            self.hparams.N_samples,
+                            self.hparams.use_disp,
+                            self.hparams.perturb,
+                            self.hparams.noise_std,
+                            self.hparams.N_importance,
+                            self.hparams.chunk,  # chunk size is effective in val mode
+                            self.train_dataset.white_back)
+
+            for k, v in rendered_ray_chunks.items():
+                results[k] += [v]
+
+        for k, v in results.items():
+            results[k] = torch.cat(v, 0)
+        return results
+
+    def setup(self, stage: str):
+        dataset = dataset_dict[self.hparams.dataset_name]
+        kwargs = {'root_dir': self.hparams.root_dir,
+                  'img_wh': tuple(self.hparams.img_wh)}
+        if self.hparams.dataset_name == 'llff':
+            kwargs['spheric_poses'] = self.hparams.spheric_poses
+            kwargs['val_num'] = self.hparams.num_gpus
+        self.train_dataset = dataset(split='train', **kwargs)
+        self.val_dataset = dataset(split='val', **kwargs)
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset,
+                          shuffle=True,
+                          num_workers=4,
+                          batch_size=self.hparams.batch_size,
+                          pin_memory=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset,
+                          shuffle=False,
+                          num_workers=4,
+                          batch_size=1,  # validate one image (H*W rays) at a time
+                          pin_memory=True)
+
+    def configure_optimizers(self):
+        self.optimizer = get_optimizer(self.hparams, self.models)
+        scheduler = get_scheduler(self.hparams, self.optimizer)
+        return [self.optimizer], [scheduler]
+
+    def training_step(self, batch, batch_nb):
+        rays, rgbs = batch['rays'], batch['rgbs']
+        results = self(rays)
+        loss = self.loss(results, rgbs)
+
+        with torch.no_grad():
+            typ = 'fine' if 'rgb_fine' in results else 'coarse'
+            psnr_ = psnr(results[f'rgb_{typ}'], rgbs)
+
+        log = {'lr': get_learning_rate(self.optimizer), 'train/loss': loss, 'train/psnr': psnr_}
+        self.log_dict(log, on_epoch=True, prog_bar=True)
+
+        return loss
+
+    def validation_step(self, batch, batch_nb):
+        rays, rgbs = batch['rays'], batch['rgbs']
+        rays = rays.squeeze()  # (H*W, 3)
+        rgbs = rgbs.squeeze()  # (H*W, 3)
+        results = self(rays)
+        loss = self.loss(results, rgbs)
+        typ = 'fine' if 'rgb_fine' in results else 'coarse'
+
+        if batch_nb == 0:
+            W, H = self.hparams.img_wh
+            img = results[f'rgb_{typ}'].view(H, W, 3).permute(2, 0, 1).cpu()  # (3, H, W)
+            img_gt = rgbs.view(H, W, 3).permute(2, 0, 1).cpu()  # (3, H, W)
+            depth = visualize_depth(results[f'depth_{typ}'].view(H, W))  # (3, H, W)
+            stack = torch.stack([img_gt, img, depth])  # (3, 3, H, W)
+            self.logger.experiment.add_images('val/GT_pred_depth',
+                                              stack, self.global_step)
+
+        psnr_ = psnr(results[f'rgb_{typ}'], rgbs)
+        log = {'val_loss': loss, 'val_psnr': psnr_}
+        self.log_dict(log, on_epoch=True, prog_bar=True)
+        self.val_outputs['val_loss'].append(loss)
+        self.val_outputs['val_psnr'].append(psnr_)
+        return loss
+
+    def on_validation_epoch_end(self):
+        # mean_loss = torch.stack([x for x in self.val_outputs['val_loss']]).mean()
+        # mean_psnr = torch.stack([x for x in self.val_outputs['val_psnr']]).mean()
+        mean_loss = torch.mean(torch.tensor(self.val_outputs['val_loss']))
+        mean_psnr = torch.mean(torch.tensor(self.val_outputs['val_psnr']))
+        for key in self.val_outputs:
+            self.val_outputs[key] = []  # 清空每个键对应的列表，将其设置为空列表
+
+        log = {'val/loss': mean_loss, 'val/psnr': mean_psnr}
+        # self.log('val/loss', mean_loss)
+        # self.log('val/psnr', mean_psnr, prog_bar=True)
+        self.log_dict(log, prog_bar=True)
+
+
+def main(hparams):
+    # nerf_data = NeRFDataModule(hparams=hparams)
+    nerf_pl = NeRFLightningModule(hparams)
+    ckpt_cb = ModelCheckpoint(dirpath=f'ckpts/{hparams.exp_name}',
+                              filename='model-{epoch:02d}-{val/psnr:.2f}',
+                              monitor='val/psnr',
+                              mode='max',
+                              save_top_k=1)
+    pbar = TQDMProgressBar(refresh_rate=1)
+    callbacks = [ModelSummary(max_depth=-1), ckpt_cb, pbar]
+
+    logger = TensorBoardLogger(save_dir="logs",
+                               name=hparams.exp_name,
+                               default_hp_metric=False)
+
+    trainer = Trainer(max_epochs=hparams.num_epochs,
+                      callbacks=callbacks,
+                      # resume_from_checkpoint=hparams.ckpt_path,
+                      logger=logger,
+                      enable_model_summary=False,
+                      accelerator='auto',
+                      devices=hparams.num_gpus,
+                      num_sanity_val_steps=1,
+                      benchmark=True,
+                      profiler="simple" if hparams.num_gpus == 1 else None)
+
+    # trainer.fit(nerf_pl, datamodule=nerf_data, ckpt_path=hparams.ckpt_path)
+    trainer.fit(nerf_pl, ckpt_path=hparams.ckpt_path)
+    # trainer.fit(nerf_pl)
+
+
+
+if __name__ == '__main__':
+    hparams = opt.get_opts()
+    main(hparams)
---
+++ utils/utils.py
@@ -0,0 +1,137 @@
+import torch
+from torch.optim import SGD, Adam
+import torch.optim as optim
+from torch.optim.lr_scheduler import CosineAnnealingLR, MultiStepLR, LambdaLR
+# from .warmup_scheduler import GradualWarmupScheduler
+from kornia.losses import ssim as dssim
+import cv2
+import numpy as np
+from PIL import Image
+import torchvision.transforms as T
+
+
+# 提取模型参数信息
+def extract_model_state_dict(ckpt_path, model_name='model', prefixes_to_ignore=[]):
+    checkpoint = torch.load(ckpt_path, map_location=torch.device('cpu'))
+    checkpoint_ = {}
+    if 'state_dict' in checkpoint: # if it's a pytorch-lightning checkpoint
+        checkpoint = checkpoint['state_dict']
+    for k, v in checkpoint.items():
+        if not k.startswith(model_name):
+            continue
+        k = k[len(model_name)+1:]
+        for prefix in prefixes_to_ignore:
+            if k.startswith(prefix):
+                print('ignore', k)
+                break
+        else:
+            checkpoint_[k] = v
+    return checkpoint_
+
+
+#加载检查点
+def load_ckpt(model, ckpt_path, model_name='model', prefixes_to_ignore=[]):
+    if not ckpt_path:
+        return
+    model_dict = model.state_dict()
+    checkpoint_ = extract_model_state_dict(ckpt_path, model_name, prefixes_to_ignore)
+    model_dict.update(checkpoint_)
+    model.load_state_dict(model_dict)
+
+
+# 获得参数
+def get_parameters(models):
+    """Get all model parameters recursively."""
+    parameters = []
+    if isinstance(models, list):
+        for model in models:
+            parameters += get_parameters(model)
+    elif isinstance(models, dict):
+        for model in models.values():
+            parameters += get_parameters(model)
+    else: # models is actually a single pytorch model
+        parameters += list(models.parameters())
+    return parameters
+
+
+# 得到优化器
+def get_optimizer(hparams, models):
+    eps = 1e-8
+    parameters = get_parameters(models)
+    if hparams.optimizer == 'sgd':
+        optimizer = SGD(parameters, lr=hparams.lr,
+                        momentum=hparams.momentum, weight_decay=hparams.weight_decay)
+    elif hparams.optimizer == 'adam':
+        optimizer = Adam(parameters, lr=hparams.lr, eps=eps,
+                         weight_decay=hparams.weight_decay)
+    elif hparams.optimizer == 'radam':
+        optimizer = optim.RAdam(parameters, lr=hparams.lr, eps=eps,
+                                weight_decay=hparams.weight_decay)
+    elif hparams.optimizer == 'ranger':
+        optimizer = optim.Ranger(parameters, lr=hparams.lr, eps=eps,
+                                 weight_decay=hparams.weight_decay)
+    else:
+        raise ValueError('optimizer not recognized!')
+
+    return optimizer
+
+
+# 调整学习率
+def get_scheduler(hparams, optimizer):
+    eps = 1e-8
+    if hparams.lr_scheduler == 'steplr':
+        scheduler = MultiStepLR(optimizer, milestones=hparams.decay_step,
+                                gamma=hparams.decay_gamma)
+    elif hparams.lr_scheduler == 'cosine':
+        scheduler = CosineAnnealingLR(optimizer, T_max=hparams.num_epochs, eta_min=eps)
+    elif hparams.lr_scheduler == 'poly':
+        scheduler = LambdaLR(optimizer,
+                             lambda epoch: (1-epoch/hparams.num_epochs)**hparams.poly_exp)
+    else:
+        raise ValueError('scheduler not recognized!')
+
+    # if hparams.warmup_epochs > 0 and hparams.optimizer not in ['radam', 'ranger']:
+    #     scheduler = GradualWarmupScheduler(optimizer, multiplier=hparams.warmup_multiplier,
+    #                                        total_epoch=hparams.warmup_epochs, after_scheduler=scheduler)
+
+    return scheduler
+
+
+def get_learning_rate(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+
+
+def mse(image_pred, image_gt, valid_mask=None, reduction='mean'):
+    value = (image_pred-image_gt)**2
+    if valid_mask is not None:
+        value = value[valid_mask]
+    if reduction == 'mean':
+        return torch.mean(value)
+    return value
+
+
+def psnr(image_pred, image_gt, valid_mask=None, reduction='mean'):
+    return -10*torch.log10(mse(image_pred, image_gt, valid_mask, reduction))
+
+
+def ssim(image_pred, image_gt, reduction='mean'):
+    """
+    image_pred and image_gt: (1, 3, H, W)
+    """
+    dssim_ = dssim(image_pred, image_gt, 3, reduction) # dissimilarity in [0, 1]
+    return 1-2*dssim_ # in [-1, 1]
+
+def visualize_depth(depth, cmap=cv2.COLORMAP_JET):
+    """
+    depth: (H, W)
+    """
+    x = depth.cpu().numpy()
+    x = np.nan_to_num(x) # change nan to 0
+    mi = np.min(x) # get minimum depth
+    ma = np.max(x)
+    x = (x-mi)/max(ma-mi, 1e-8) # normalize to 0~1
+    x = (255*x).astype(np.uint8)
+    x_ = Image.fromarray(cv2.applyColorMap(x, cmap))
+    x_ = T.ToTensor()(x_) # (3, H, W)
+    return x_
